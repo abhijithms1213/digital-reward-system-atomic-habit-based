6f609fbe74b9ecdcda571ec3a547285ac416833e
abhijithms1213 <abhijithms1213@gmail.com>
2026-01-16 09:03:41 +0530

    studying bloc

---
+++ android/app/src/main/kotlin/com/example/drewardsystem/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.drewardsystem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:device_preview_plus/device_preview_plus.dart';
+import 'package:drewardsystem/Home/bloc/home_bloc.dart';
+import 'package:drewardsystem/Home/page.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(DevicePreview(builder:(context) => const MyApp(),enabled: !kReleaseMode,));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'First Method',
+          // You can use the library anywhere in the app even in theme
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          home: child,
+        );
+      },
+      child:  BlocProvider(
+        create: (context) => HomeBloc(),
+        child: MyHomePage()
+      )
+    );
+  }
+}
+
