1b20afe0f23d8f336f1a47d4f9afde8043b759b0
abhijithms1213 <abhijithms1213@gmail.com>
2026-02-04 12:00:40 +0530

    feat: :sparkles: added new assets for habits multi

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,17 +1,27 @@
 class AppAssets {
+   // Music
    static const String sfxRise='assets/audio/sfx_01.mp3';
    static const String sfxUpVolumeRise='assets/audio/sfx_01_upVolume.MP3';
    static const String sfxEnterButton='assets/audio/enter_button.MP3';
    static const String sfxEarn='assets/audio/sfx_02.mp3';
    
-   static const String blackSmithLottie='assets/lottie/Button Animation.json';
-   static const String circleGrowLottie='assets/lottie/Circle grow.json';
    static const String confettiLottie='assets/lottie/Confetti.json';
    static const String successLottie='assets/lottie/Success.json';
    static const String trophyLottie='assets/lottie/Trophy.json';
-   static const String exerciseDietLottie='assets/lottie/exercise_for_diet.json';
-   static const String stressLottie='assets/lottie/Stress Management.json';
+   static const String exerciseDietLottie='assets/lottie/exercise_for_diet.json';//dumble checkbox
+
+   // hammer need to be removed a bug while deleting
    static const String hammerLottie='assets/lottie/hammer.json';
+
    static const String pointEarnLottie='assets/lottie/points.json';
-   static const String tapEffectLottie='assets/lottie/tap_confetti_effect.json';
+   static const String tapEffectLottie='assets/lottie/tap_confetti_effect.json'; //tap effect confetti
+   // Effects
+   static const String waterSplashLiftLottie='assets/lottie/Water splash effect.json'; // hold first time animation
+  //  static const String holdTapEffectLottie='assets/lottie/Confetti explode.json';  // hold animation for ripple effect
+   // Modes
+   static const String readingLottie='assets/lottie/reading.json'; // reading habit
+   static const String gymWeightLiftLottie='assets/lottie/Weightlifting competition.json';
+   static const String gymLoadingBasedLottie='assets/lottie/Kaizen Gym Animation.json'; // dumbel loading like
+   static const String developerLottie='assets/lottie/Developer_Yoga.json'; // developer
+   static const String stressLottie='assets/lottie/Stress Management.json'; //meditation 
 }
