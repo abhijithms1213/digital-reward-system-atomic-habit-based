8594a4ca2bba7987b1a63d110414807cff2af2fe
abhijithms1213 <abhijithms1213@gmail.com>
2026-02-06 08:41:37 +0530

    feat: :sparkles: added new assets
    ok
    
    nop

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -4,19 +4,19 @@ class AppAssets {
    static const String sfxUpVolumeRise='assets/audio/sfx_01_upVolume.MP3';
    static const String sfxEnterButton='assets/audio/enter_button.MP3';
    static const String sfxEarn='assets/audio/sfx_02.mp3';
+   static const String sfxMiniEnterForMiniTasks='assets/audio/sfx-button_press-enter.mp3';
+   static const String sfxMiniEarnSound='assets/audio/sfx-pop.mp3';
    
    static const String confettiLottie='assets/lottie/Confetti.json';
    static const String successLottie='assets/lottie/Success.json';
    static const String trophyLottie='assets/lottie/Trophy.json';
    static const String exerciseDietLottie='assets/lottie/exercise_for_diet.json';//dumble checkbox
 
-   // hammer need to be removed a bug while deleting
-   static const String hammerLottie='assets/lottie/hammer.json';
-
    static const String pointEarnLottie='assets/lottie/points.json';
    static const String tapEffectLottie='assets/lottie/tap_confetti_effect.json'; //tap effect confetti
    // Effects
    static const String waterSplashLiftLottie='assets/lottie/Water splash effect.json'; // hold first time animation
+   static const String flextConfettiLottie='assets/lottie/Flex Confetti.json';
   //  static const String holdTapEffectLottie='assets/lottie/Confetti explode.json';  // hold animation for ripple effect
    // Modes
    static const String readingLottie='assets/lottie/reading.json'; // reading habit
@@ -24,4 +24,18 @@ class AppAssets {
    static const String gymLoadingBasedLottie='assets/lottie/Kaizen Gym Animation.json'; // dumbel loading like
    static const String developerLottie='assets/lottie/Developer_Yoga.json'; // developer
    static const String stressLottie='assets/lottie/Stress Management.json'; //meditation 
+   static const String nightLottie='assets/lottie/cat halloween.json'; //meditation 
+   static const String morningLottie='assets/lottie/Task Done.json'; //meditation 
+   static const String coffeeMorningLottie='assets/lottie/coffee-break.json'; //meditation 
+   static const String tapBurstLottie='assets/lottie/Tap Burst.json'; //meditation 
+
+   
+   static const String aimHitLottie='assets/lottie/Aim hit.json'; // archery hit in middle
+
+   static const String sandyLoadingLottie='assets/lottie/Sand-Loading.json';
+
+   // MiniTasks
+   static const String success01Lottie='assets/lottie/success_01.json';
+   static const String success02Lottie='assets/lottie/success_02.json';
+   static const String success03Lottie='assets/lottie/success_03.json';
 }
